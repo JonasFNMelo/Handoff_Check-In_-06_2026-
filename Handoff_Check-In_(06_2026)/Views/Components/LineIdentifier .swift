bad9e72df6811e985f0d66c4aea8aff7a638693a
Jonas Melo <jonasFNMelo@gmail.com>
2026-06-22 16:18:55 -0300

    feat: lineIdentifier finished

--- Handoff_Check-In_(06_2026)/Views/Components/LineIdentifier .swift
+++ Handoff_Check-In_(06_2026)/Views/Components/LineIdentifier .swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct LineIdentifier_: View {
+    let lineNumber: String
+    let lineColor:  Color
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(lineNumber)
+            .foregroundStyle(.foregroundWhite)
+            .bold()
+            .font(.system(size: 16))
+            .background{
+                RoundedRectangle(cornerRadius: 8)
+                    .frame(width: 90,height: 44)
+                    .foregroundStyle(lineColor)
+            }
     }
 }
 
 #Preview {
-    LineIdentifier_()
+    let lineNumber = "6913-10"
+    let lineColor: Color = .zoningLightBlue
+    LineIdentifier_(lineNumber: lineNumber, lineColor: lineColor)
 }
