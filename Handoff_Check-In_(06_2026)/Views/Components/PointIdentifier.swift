0ed77b8c14b0a7a3825947d8de49fe9fb1bd3078
Jonas Melo <jonasFNMelo@gmail.com>
2026-06-22 16:11:20 -0300

    feat: creation of components files

---
+++ Handoff_Check-In_(06_2026)/Views/Components/PointIdentifier.swift
@@ -0,0 +1,18 @@
+//
+//  PointIdentifier.swift
+//  Handoff_Check-In_(06_2026)
+//
+//  Created by Jonas Fernando Nascimento Melo on 22/06/26.
+//
+
+import SwiftUI
+
+struct PointIdentifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PointIdentifier()
+}
