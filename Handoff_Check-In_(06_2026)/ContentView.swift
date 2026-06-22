351a669cfd9af9929a91a1d17db1689531755d08
Jonas Melo <jonasFNMelo@gmail.com>
2026-06-22 15:48:37 -0300

    feat: creation of the project

---
+++ Handoff_Check-In_(06_2026)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Handoff_Check-In_(06_2026)
+//
+//  Created by Jonas Fernando Nascimento Melo on 22/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Handoff_Check-In_(06_2026)/Handoff_Check_In__06_2026_App.swift
@@ -0,0 +1,17 @@
+//
+//  Handoff_Check_In__06_2026_App.swift
+//  Handoff_Check-In_(06_2026)
+//
+//  Created by Jonas Fernando Nascimento Melo on 22/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Handoff_Check_In__06_2026_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
